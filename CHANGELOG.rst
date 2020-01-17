67fa4b67bff6701078a8ef4cfaf9e35bf9daff1e
Shekhar <pythonic@gmail.com>
2020-01-17 16:55:13 +0530

    - Added CHANGELOG - Test data

---
+++ CHANGELOG.rst
@@ -0,0 +1,10 @@
+=======
+History
+=======
+
+0.3.0 (2020-01-17)
+------------------
+
+* Added support for link mark and mark atrributes
+* Added BlockQuote and Hardbreak
+* Used Black formatter
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tiptapy",
-    version="0.0.1",
+    version="0.3.0",
     url="https://github.com/scrolltech/tiptapy",
     description="Library that generates HTML output from JSON export of tiptap editor",
     long_description=open("README.md").read(),
