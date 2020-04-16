f9294dd5763925d208ba44b0fe12630d119bb3e2
Shekhar <pythonic@gmail.com>
2020-04-16 17:41:31 +0530

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tiptapy",
-    version="0.5.0",
+    version="0.5.1",
     url="https://github.com/scrolltech/tiptapy",
     description="Library that generates HTML output from JSON export of tiptap editor",
     long_description=open("README.md").read(),
