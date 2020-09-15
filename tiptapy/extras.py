ba6e75a90682f43c65b8fb32c105f44bd4fad8e0
Pradhvan <pradhvanbisht@gmail.com>
2020-09-15 16:30:01 +0530

    Adds Audio Block
    * Added `StackAudio(BaseContainer)` to handel `type=audio`
    * Added `templates` to store `stack-audio-playes.html`
    * Added tests for StackAudio
    
    Removed string manipulation while reading audio player template file
    
    Updated testdata for audio tag

--- tiptapy/extras.py
+++ tiptapy/extras.py
@@ -1,4 +1,5 @@
-from . import Image, register_renderer
+import re
+from . import BaseNode, Image, register_renderer, e
 
 
 class FeaturedImage(Image):
@@ -6,4 +7,28 @@ class FeaturedImage(Image):
     css_class = "featured-image"
 
 
+class StackAudio(BaseNode):
+    type = "audio"
+    wrap_tag = "figure"
+
+    def is_renderable(self, node):
+        attrs = node.get("attrs", {})
+        return bool(attrs.get("src", ""))
+
+    def inner_render(self, node):
+        attrs = node["attrs"]
+        src = attrs.get('src', '').strip()
+        caption = attrs.get('caption', '').strip()
+        audio_link = f'<audio src={src}></audio>'
+        with open('tiptapy/templates/stack-audio-player.html', 'r') as f:
+            data = f.read()
+        # Removing whitespaces between html tags.
+        audio_player_block = re.sub(r'\s\s+', '', data)
+        html = f'<div>{audio_link}{audio_player_block}</div>'
+        if caption:
+            html = html + f'<figcaption>{e(caption)}</figcaption>'
+        return html
+
+
 register_renderer(FeaturedImage)
+register_renderer(StackAudio)
