a9178a0729a0a49f77e0bd2778da091b310df74c
Arijit Basu <sayanarijit@gmail.com>
2024-02-12 16:19:38 +0530

    Cleanup

--- tiptapy/__init__.py
+++ tiptapy/__init__.py
@@ -71,7 +71,7 @@ def escape_values_recursive(node):
         for k, v in node.items():
             esc_k = escape(k)
             if k != esc_k:
-                node[esc_k] = node.pop(k)
+                node.pop(k)
             if esc_k == html_key:
                 # Allow only iframe tag
                 p = IFrameParser()
